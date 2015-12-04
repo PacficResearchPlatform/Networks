8112f64451a1720c260438f16c9cc626f1210894
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2015-12-03 22:11:12 -0800

    More information on print

--- iptables-gen.py
+++ iptables-gen.py
@@ -2,9 +2,10 @@
 # Proof of principle to convert network conf file to iptables entries
 import json
 import sys
-def genEntry(network):
-	s = "-A INPUT -p tcp --source %s -j ACCEPT\n" % network
-	s = s + "-A INPUT -p udp --source %s -j ACCEPT" % network
+def genEntry(network,description):
+	s = "## %s\n" % description
+	s = s + "-A INPUT -p tcp --source %s --dport 1024:65535 -j ACCEPT\n" % network
+	s = s + "-A INPUT -p udp --source %s --dport 1024:65535 -j ACCEPT" % network
 	return s
 gen6="-6" in sys.argv
 if gen6:
@@ -21,9 +22,10 @@ for f in sys.argv[1:]:
 	fh.close()
 	for group in nets:
 		for sn in group['subnets']:
+			desc = sn['name']	
 			subnet = sn['subnet']
 			type =  subnet['type']
 			network = subnet['network']
 			if type.lower() == ttype :
-				print genEntry(network)
+				print genEntry(network,desc)
 
